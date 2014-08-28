92a51feecf0fd33695b0abfa82204ba521568744
Daniel Naranjo <dnaranjo@gmail.com>
2014-08-28 13:38:06 -0430

    Cambios

--- platforms/ios/www/js/services.js
+++ platforms/ios/www/js/services.js
@@ -3,36 +3,20 @@ angular.module('starter.services', [])
 /**
  * A simple example service that returns some data.
  */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff', address: 'Dublin, Ireland' },
-    { id: 1, name: 'G.I. Joe', address: 'New York City, NY' },
-    { id: 2, name: 'Miss Frizzle', address: 'Manila Philipines' },
-    { id: 3, name: 'Ash Ketchum', address: 'Buenos Aires, Argentina' }
-  ];
-
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
-    }
-  }
-})
 .factory('Bares', function() {
-  // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
   var bares = [
-    { id: 0, name: 'La trucha azul', address: 'Dublin, Ireland', logo:'img/img1.jpg', stars:5 },
+    { id: 0, name: 'La trucha azul', address: 'Dublin, Ireland', logo:'img/img1.jpg', stars:1 },
     { id: 1, name: 'El Corte Ingles', address: 'New York City, NY', logo:'img/img2.jpg', stars:5 },
-    { id: 2, name: 'Coyote Ugly', address: 'Manila Philipines', logo:'img/img3.jpg', stars:5 },
-    { id: 3, name: 'Kokomo', address: 'Buenos Aires, Argentina', logo:'img/img4.jpg', stars:5 }
+    { id: 2, name: 'Coyote Ugly', address: 'Manila Philipines', logo:'img/img3.jpg', stars:2 },
+    { id: 3, name: 'Kokomo', address: 'Buenos Aires, Argentina', logo:'img/img4.jpg', stars:4 },{ id: 0, name: 'La trucha azul', address: 'Dublin, Ireland', logo:'img/img1.jpg', stars:1 },
+    { id: 4, name: 'El Corte Ingles', address: 'New York City, NY', logo:'img/img2.jpg', stars:5 },
+    { id: 5, name: 'Coyote Ugly', address: 'Manila Philipines', logo:'img/img3.jpg', stars:2 },
+    { id: 6, name: 'Kokomo 4', address: 'Buenos Aires, Argentina', logo:'img/img4.jpg', stars:4 },{ id: 0, name: 'La trucha azul', address: 'Dublin, Ireland', logo:'img/img1.jpg', stars:1 },
+    { id: 7, name: 'El Corte Ingles 3', address: 'New York City, NY', logo:'img/img2.jpg', stars:5 },
+    { id: 8, name: 'Coyote Ugly 2', address: 'Manila Philipines', logo:'img/img3.jpg', stars:2 },
+    { id: 9, name: 'Kokomo 2', address: 'Caracas, Venezuela', logo:'img/img4.jpg', stars:4 }
+
   ];
 
   return {
