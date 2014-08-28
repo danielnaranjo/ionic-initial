7001fdb08f538807b402a1f9669758096df74074
Daniel Naranjo <dnaranjo@gmail.com>
2014-08-28 14:15:08 -0430

    More data for test propuse

--- www/js/services.js
+++ www/js/services.js
@@ -6,16 +6,18 @@ angular.module('starter.services', [])
 .factory('Bares', function() {
 
   var bares = [
-    { id: 0, name: 'La trucha azul', address: 'Dublin, Ireland', logo:'img/img1.jpg', stars:1 },
-    { id: 1, name: 'El Corte Ingles', address: 'New York City, NY', logo:'img/img2.jpg', stars:5 },
-    { id: 2, name: 'Coyote Ugly', address: 'Manila Philipines', logo:'img/img3.jpg', stars:2 },
-    { id: 3, name: 'Kokomo', address: 'Buenos Aires, Argentina', logo:'img/img4.jpg', stars:4 },{ id: 0, name: 'La trucha azul', address: 'Dublin, Ireland', logo:'img/img1.jpg', stars:1 },
-    { id: 4, name: 'El Corte Ingles', address: 'New York City, NY', logo:'img/img2.jpg', stars:5 },
-    { id: 5, name: 'Coyote Ugly', address: 'Manila Philipines', logo:'img/img3.jpg', stars:2 },
-    { id: 6, name: 'Kokomo 4', address: 'Buenos Aires, Argentina', logo:'img/img4.jpg', stars:4 },{ id: 0, name: 'La trucha azul', address: 'Dublin, Ireland', logo:'img/img1.jpg', stars:1 },
-    { id: 7, name: 'El Corte Ingles 3', address: 'New York City, NY', logo:'img/img2.jpg', stars:5 },
-    { id: 8, name: 'Coyote Ugly 2', address: 'Manila Philipines', logo:'img/img3.jpg', stars:2 },
-    { id: 9, name: 'Kokomo 2', address: 'Caracas, Venezuela', logo:'img/img4.jpg', stars:4 }
+    { id: 0, name: 'La trucha azul', address: 'Dublin, Ireland', logo:'img/img1.jpg', stars:1, favorite: false },
+    { id: 1, name: 'El Corte Ingles', address: 'New York City, NY', logo:'img/img2.jpg', stars:5, favorite: false },
+    { id: 2, name: 'Coyote Ugly', address: 'Manila Philipines', logo:'img/img3.jpg', stars:2, favorite: false },
+    { id: 3, name: 'Kokomo', address: 'Buenos Aires, Argentina', logo:'img/img4.jpg', stars:4, favorite: false },
+    { id: 0, name: 'La trucha azul', address: 'Dublin, Ireland', logo:'img/img1.jpg', stars:1, favorite: false },
+    { id: 4, name: 'El Corte Ingles', address: 'New York City, NY', logo:'img/img2.jpg', stars:5, favorite: false },
+    { id: 5, name: 'Coyote Ugly', address: 'Manila Philipines', logo:'img/img3.jpg', stars:2, favorite: false },
+    { id: 6, name: 'Kokomo 4', address: 'Buenos Aires, Argentina', logo:'img/img4.jpg', stars:4, favorite: true },
+    { id: 0, name: 'La trucha azul', address: 'Dublin, Ireland', logo:'img/img1.jpg', stars:1, favorite: false },
+    { id: 7, name: 'El Corte Ingles 3', address: 'New York City, NY', logo:'img/img2.jpg', stars:5, favorite: false },
+    { id: 8, name: 'Coyote Ugly 2', address: 'Manila Philipines', logo:'img/img3.jpg', stars:2, favorite: true },
+    { id: 9, name: 'Kokomo 2', address: 'Caracas, Venezuela', logo:'img/img4.jpg', stars:4, favorite: true }
 
   ];
 
