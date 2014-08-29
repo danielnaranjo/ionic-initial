ed2bcde5661c622b2d1ac7812695c706c7fc3a85
Daniel Naranjo <dnaranjo@gmail.com>
2014-08-28 21:36:23 -0430

    Clean

--- www/js/filters.js
+++ www/js/filters.js
@@ -1,12 +1,10 @@
 angular.module('starter.filters', []).filter('ranking', function() {
+	// http://stackoverflow.com/questions/11873570/angularjs-for-loop-with-numbers-ranges
 	return function(input){
-	// <i class="icon ion-heart" ng-show="bar.favorite"></i>
-	var range = [],
-		estrellas="<i class=\"ion-ios7-star\" ng-show=\"bar.stars\"></i>";
+	var range = [];
 	for(var i=0;i<input;i++) {
 		range.push(i);
 	}
     return range;
-    //return input ? '\f1e0' : '\f1df';
-  };
+  	};
 });
